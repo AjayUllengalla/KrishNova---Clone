ff805a58a12bfdc4047dc22c6d1ae001669bbede
Ajay Ullengalla <ullengallaajay28@gmail.com>
2026-02-18 23:48:53 +0530

    left side part added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,19 @@ import React from "react";
 function About () {
     return(
         <>
-        <section className="mt-4">
+        <section className="about-section">
             <div className="container">
-                
+                <div className="row">
+                    {/* left-side part  */}
+                    <div className="col-md-6">
+                        <h2 className="text-white">About KrishNova LLP</h2>
+                        <p className="text-dim-color">Founded in 2024, KrishNova LLP is a hub for technology, learning and product incubation. We design industry-aligned courses and deliver hands-on capstone projects to help learners and teams ship.</p>
+                        <p className="text-dim-color semi-bold">• AI/ML & Data Science</p>
+                        <p className="fs-6 text-dim-color">•  Cloud & DevOps</p>
+                        <p className="fs-6 text-dim-color">• Cybersecurity & Blockchain</p>
+                        <p className="fs-6 text-dim-color">•  Full-Stack & IoT</p>
+                    </div>
+                </div>
             </div>
         </section>
         </>
