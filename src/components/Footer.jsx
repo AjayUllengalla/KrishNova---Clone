a621b5ebba86cb515af94162bd44844caca72281
Ajay Ullengalla <ullengallaajay28@gmail.com>
2026-03-02 12:59:39 +0530

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <>
+      <section className="cta-section py-5">
+  <div className="container">
+    <div className="cta-box d-flex flex-column flex-md-row justify-content-between align-items-center">
+
+      {/* Left Text */}
+      <div>
+        <p className="cta-small mb-2">
+          Ready to build something impactful?
+        </p>
+        <h3 className="cta-main mb-0">
+          Partner with KrishNova for developments, products, and trainings.
+        </h3>
+      </div>
+
+      {/* Right Button */}
+      <div className="mt-4 mt-md-0">
+        <a href="#contact" className="btn cta-btn">
+          Let's collaborate
+        </a>
+      </div>
+
+    </div>
+  </div>
+</section>
+        </>
+    )
+}
+
+export default Footer;
